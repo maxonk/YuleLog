2ec6771ed231926e59441cd2e2509392f34a19fa
Kevin Maxon <kevin.maxon@gmail.com>
2016-12-06 20:59:46 -0800

    about to make flames narrower

--- Assets/Flame.cs
+++ Assets/Flame.cs
@@ -6,7 +6,7 @@ public class Flame : MonoBehaviour {
     public Gradient gradient;
     public AnimationCurve animCurve;
 
-    const int VERTEX_COUNT = 12;
+    const int FLAME_COUNT = 10;
 
     Vector3[] vertices;
     Vector3[] flamePositions;
@@ -16,33 +16,33 @@ public class Flame : MonoBehaviour {
     Mesh mesh;
 
 	void Awake () {
-        flameHeights = new float[VERTEX_COUNT - 2];
+        flameHeights = new float[FLAME_COUNT];
 
         mesh = new Mesh();
         GetComponent<MeshFilter>().sharedMesh = mesh;
 
-        vertices = new Vector3[VERTEX_COUNT];
-        flamePositions = new Vector3[VERTEX_COUNT];
+        vertices = new Vector3[FLAME_COUNT + 2];
+        flamePositions = new Vector3[FLAME_COUNT + 2];
         flamePositions[0] = Vector3.left * 3;
-        flamePositions[VERTEX_COUNT - 1] = Vector3.right * 3;
-        for (int i = 0; i < VERTEX_COUNT - 2; i++) {
-            flamePositions[i + 1] = Vector3.Lerp(Vector3.left, Vector3.right, i / (float)(VERTEX_COUNT - 2)) * 4;
+        flamePositions[FLAME_COUNT + 1] = Vector3.right * 3;
+        for (int i = 0; i < FLAME_COUNT; i++) {
+            flamePositions[i + 1] = Vector3.Lerp(Vector3.left, Vector3.right, i / (float)(FLAME_COUNT)) * 4;
         }
         mesh.vertices = flamePositions;
 
-        var triangles = new int[(VERTEX_COUNT - 2) * 6];
-        for(int i = 0, v = 1; v < VERTEX_COUNT - 1; i += 6, v++) {
+        var triangles = new int[FLAME_COUNT * 6];
+        for(int i = 0, v = 1; v < FLAME_COUNT + 1; i += 6, v++) {
             triangles[i] = 0;
             triangles[i + 1] = v;
-            triangles[i + 2] = VERTEX_COUNT - 1;
+            triangles[i + 2] = FLAME_COUNT + 1;
             triangles[i + 5] = 0;
             triangles[i + 4] = v;
-            triangles[i + 3] = VERTEX_COUNT - 1;
+            triangles[i + 3] = FLAME_COUNT + 1;
         }
         mesh.triangles = triangles;
 
-        colors = new Color[VERTEX_COUNT];
-        for(int i = 0; i < VERTEX_COUNT; i++) {
+        colors = new Color[FLAME_COUNT + 2];
+        for(int i = 0; i < FLAME_COUNT + 2; i++) {
             colors[i] = Color.black;
         }
         mesh.colors = colors;
@@ -51,36 +51,37 @@ public class Flame : MonoBehaviour {
     public void updateSimData(LogBurner.BurnSimNode[] burnSimMap, float totalFuelPct) {
         Array.Clear(flameHeights, 0, flameHeights.Length);
 
+        Vector3 leftMostV = Vector3.zero, rightMostV = Vector3.zero;
         for (int i = 0; i < burnSimMap.Length; i++) {
-            int f = Mathf.FloorToInt(i / (float)burnSimMap.Length * (VERTEX_COUNT - 2));
+            int f = Mathf.FloorToInt(i / (float)burnSimMap.Length * FLAME_COUNT);
             flameHeights[f] += burnSimMap[i].heat;
+            if (burnSimMap[i].position.x < leftMostV.x) {
+                leftMostV = burnSimMap[i].position;
+            } else if (burnSimMap[i].position.x > rightMostV.x) {
+                rightMostV = burnSimMap[i].position;
+            }
         }
 
 
         float maxHeight = 0f;
-        Vector3 leftMost = Vector3.zero, rightMost = Vector3.zero;
-        for (int i = 0; i < VERTEX_COUNT - 2; i++) {
-            flamePositions[i + 1] = Vector3.Lerp(Vector3.left, Vector3.right, i / (float)(VERTEX_COUNT - 2)) * totalFuelPct * 4;
+        for (int i = 0; i < FLAME_COUNT; i++) {
+            flamePositions[i + 1] = Vector3.Lerp(leftMostV, rightMostV, i / (float)FLAME_COUNT) * totalFuelPct;
             if (flameHeights[i] > maxHeight) maxHeight = flameHeights[i];
             //also get left and right points here
-            if (burnSimMap[i].position.x < leftMost.x) leftMost = burnSimMap[i].position;
-            if (burnSimMap[i].position.x > rightMost.x) rightMost = burnSimMap[i].position;
         }
-        Debug.Log(rightMost);//this aint workin
-        flamePositions[0] = leftMost;
-        flamePositions[VERTEX_COUNT - 1] = rightMost;
+        flamePositions[0] = leftMostV;
+        flamePositions[FLAME_COUNT + 1] = rightMostV;
         
-        for (int i = 0; i < VERTEX_COUNT - 2; i++) {
-            flamePositions[i + 1] = Vector3.Lerp(Vector3.left, Vector3.right, i / (float)(VERTEX_COUNT - 2)) * totalFuelPct * 4;
+        for (int i = 0; i < FLAME_COUNT; i++) {
             colors[i + 1] = gradient.Evaluate(flameHeights[i] / maxHeight);
         }
         mesh.colors = colors;
     }
 
     public void update(LogBurner.BurnSimNode[] burnSimMap, float totalFuelPct) {
-        Array.Copy(flamePositions, vertices, VERTEX_COUNT);
+        Array.Copy(flamePositions, vertices, FLAME_COUNT + 2);
         var localUp = transform.InverseTransformDirection(Vector3.up);
-        for (int i = 0; i < VERTEX_COUNT - 2; i++) {
+        for (int i = 0; i < FLAME_COUNT; i++) {
             var curveTime = (Time.realtimeSinceStartup + (i * 0.43f)) % 1f;
             vertices[i + 1] += localUp * flameHeights[i] * animCurve.Evaluate(curveTime);
         }
